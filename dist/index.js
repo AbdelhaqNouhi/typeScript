89536d39ff21aeab5105be14840fa83ad8a5d1fb
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2023-02-20 14:42:40 +0100

    Basic types

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Basic types
+let id = 5;
+let user = "abdelhaq";
+let isHere = true;
+let x = "hello";
+let age = 24;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, 'nouhi', true];
+// Tuple
+let person = [1, 'anouhi', false];
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Basic types
+let id: number = 5
+let user: string = "abdelhaq"
+let isHere: boolean = true 
+let x: any = "hello"
+let age: number = 24
+
+let ids: number[] = [1, 2, 3, 4, 5]
+let arr: any[] = [1, 'nouhi', true]
+
+// Tuple
+let person: [number, string, boolean] = [1, 'anouhi', false]
